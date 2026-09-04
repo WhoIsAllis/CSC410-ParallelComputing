df1fb9c6a99a729da81dde7a12f488980eab9274
WhoIsAllis? <tygrone6@gmail.com>
2026-09-04 15:34:00 -0500

    Add Merge and Bubble Sort
    Added code for the merge and
    bubble sort.
    Tested code and works.

--- A1 - Sequential/sorts/sorts.c
+++ A1 - Sequential/sorts/sorts.c
@@ -3,7 +3,49 @@
 // Merge Sort 
 static void merge(int arr[], int left, int mid, int right) 
 {
-    // complete this
+    int len = right - left + 1;
+    int *tmp = malloc(len * sizeof *tmp);
+    int i = left;
+    int j = mid + 1;
+    int k = 0;
+
+    while (i <= mid && j <= right) 
+    {
+        if (arr[i] <= arr[j]) 
+        {
+            tmp[k] = arr[i];
+            i++;
+        } 
+        
+        else 
+        {
+            tmp[k] = arr[j];
+            j++;
+        }
+
+        k++;
+    }
+
+    while (i <= mid) 
+    {
+        tmp[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    while (j <= right) 
+    {
+        tmp[k] = arr[j];
+        j++;
+        k++;
+    }
+
+    for (int index = 0; index < len; index++) 
+    {
+        arr[left + index] = tmp[index];
+    }
+
+    free(tmp);
 }
 
 void mergeSort(int arr[], int left, int right) 
@@ -19,7 +61,26 @@ void mergeSort(int arr[], int left, int right)
 // Bubble Sort
 void bubbleSort(int arr[], int n) 
 {
-    // complete this
+    for (int i = 0; i < n - 1; i++) 
+    {
+        int swap = 0;
+
+        for (int j = 0; j < n - i - 1; j++) 
+        {
+            if (arr[j] > arr[j + 1]) 
+            {
+                int tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+                swap = 1;
+            }
+        }
+
+        if (!swap) 
+        {
+            break;
+        }
+    }
 }
 
 void printArray(int arr[], int n) 
