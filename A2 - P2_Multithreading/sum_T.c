fc88667935f97b37bfcb4ab0b7565d1e19a2fa0e
WhoIsAllis? <tygrone6@gmail.com>
2026-09-25 16:00:42 -0500

    Add Code for Task 1
    Adding pthreads to the arraysum
    code from A1. Added timing in the code
    to verify results when executed.
    Tested and passed.

--- A2 - P2_Multithreading/sum_T.c
+++ A2 - P2_Multithreading/sum_T.c
@@ -1,32 +1,101 @@
+// Parallel array sum using pthreads
+
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-#define SIZE 60
+#define SIZE 100000000
+#define NUM_THREADS 4
 
-int sumArray(int arr[], int size) 
+int *array;
+int threadCount = NUM_THREADS;
+long long partialSums[NUM_THREADS];
+
+void *sumArrayThread(void *arg)
 {
-    int sum = 0;
-    for (int i = 0; i < size; i++) 
+    int threadId = *(int *)arg;
+    int itemsPerThread = SIZE / threadCount;
+    int start = threadId * itemsPerThread;
+    int end = (threadId == threadCount - 1) ? SIZE : start + itemsPerThread;
+    long long sum = 0;
+
+    for (int i = start; i < end; i++) 
     {
-        sum += arr[i];
+        sum += array[i];
     }
-    return sum;
+
+    partialSums[threadId] = sum;
+    return NULL;
 }
 
-int main() 
+int main(int argc, char *argv[])
 {
-    int arr[SIZE];
-    for (int i = 0; i < SIZE; i++) {
-        arr[i] = i + 1; 
+    if (argc > 2) 
+    {
+        printf("Usage: %s [number of threads: 1-%d]\n", argv[0], NUM_THREADS);
+        return 1;
     }
 
-    clock_t start = clock();
-    int totalSum = sumArray(arr, SIZE);
-    clock_t end = clock();
-    double elapsed = (double)(end - start) / CLOCKS_PER_SEC;
+    if (argc == 2) 
+    {
+        threadCount = atoi(argv[1]);
+    }
 
-    printf("Total Sum: %d\n", totalSum);
-    printf("Elapsed Time: %.6f seconds\n", elapsed);
+    if (threadCount < 1 || threadCount > NUM_THREADS) 
+    {
+        printf("Usage: %s [Number of Threads: 1-%d]\n", argv[0], NUM_THREADS);
+        return 1;
+    }
 
+    array = malloc(SIZE * sizeof(int));
+    if (array == NULL) 
+    {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < SIZE; i++) 
+    {
+        array[i] = i + 1;
+    }
+
+    pthread_t threads[NUM_THREADS];
+    int threadIds[NUM_THREADS];
+    struct timespec startTime, endTime;
+
+    clock_gettime(CLOCK_MONOTONIC, &startTime);
+
+    for (int i = 0; i < threadCount; i++) 
+    {
+        threadIds[i] = i;
+        if (pthread_create(&threads[i], NULL, sumArrayThread, &threadIds[i]) != 0) 
+        {
+            printf("Thread creation failed.\n");
+            return 1;
+        }
+    }
+
+    for (int i = 0; i < threadCount; i++) 
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    long long totalSum = 0;
+    for (int i = 0; i < threadCount; i++) 
+    {
+        totalSum += partialSums[i];
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &endTime);
+
+    double elapsed = (endTime.tv_sec - startTime.tv_sec) +
+                     (endTime.tv_nsec - startTime.tv_nsec) / 1000000000.0;
+
+    printf("Total Sum: %lld\n", totalSum);
+    printf("Threads: %d\n", threadCount);
+    printf("Elapsed Time: %.4f seconds\n", elapsed);
+
+    free(array);
     return 0;
 }
