08473dbf593ff3af07bb9666b675792ff97d25c5
WhoIsAllis? <tygrone6@gmail.com>
2026-09-25 20:15:42 -0500

    Do Task 2
    Developed solution for Task 2
    of A2 part 2. Also added in timing
    function to use for the assignment.
    Built and tested properly.

--- A2 - P2_Multithreading/matrixT.h
+++ A2 - P2_Multithreading/matrixT.h
@@ -1,30 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h> // For malloc() and free()
+#ifndef MATRIX_T_H
+#define MATRIX_T_H
 
-#define N 10 // Adjust this to test larger matrix sizes
+#define N 1000
+#define NUM_THREADS 4
 
-void displayMatrix(int** matrix, int n) 
-{
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-}
+int **allocateMatrix(void);
+void freeMatrix(int **matrix);
+void *matrixMultiplyThread(void *arg);
 
-void matrixMultiply(int** A, int** B, int** C, int n) 
-{
-    for (int i = 0; i < n; i++) 
-    {
-        for (int j = 0; j < n; j++)
-        {
-            int sum = 0;
-            for (int k = 0; k < n; k++)
-            {
-                sum += A[i][k] * B[k][j];
-            }
-            C[i][j] = sum;
-        }
-    }
-}
+#endif
